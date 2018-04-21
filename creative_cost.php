dd5d5ed59a339cd881fb78fbe75b9babe209b9cc
Tims Framework <tim@netsynced.com>
2018-04-22 00:20:59 +0100

    remove carriage returns

--- creative_cost.php
+++ creative_cost.php
@@ -1,20 +1,20 @@
 <?php
-	ini_set("display_errors", 1);	
-	header('Content-Type: application/json');	
-	$csv = array_map('str_getcsv', file('cost.csv'));
+	ini_set("display_errors", 1);
+	header('Content-Type: application/json');
+	$csv = array_map('str_getcsv', file('cost.csv'));
 	// 0 = platform, 1 = format, 2 = feature, 3 = cost	
-	$json_string = '{"creatives":[';	
+	$json_string = '{"creatives":[';
 	foreach($csv as $json)
 	{
 		$json_string .= '{"platform":"'.$json[0].'","format":"'.$json[1].'","feature":"'.$json[2].'","cost":';
-		if(!is_numeric($json[3])){
+		if(!is_numeric($json[3]))
+		{
 			$json_string .= "$json[3]";
 		}else{
 			$json_string .= $json[3];
-		}
-		
+		}		
 		$json_string .= "},";
-	}	
-	$json_string = substr($json_string, 0, -1);	
-	echo trim(rtrim($json_string . "]}"));	
+	}
+	$json_string = substr($json_string, 0, -1);
+	echo trim(rtrim($json_string . "]}"));
 ?>
